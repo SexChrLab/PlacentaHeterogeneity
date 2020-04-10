ee68cb861419c2c1fde27d050427cf25f5459bf3
Tanya Phung <10180091+tanyaphung@users.noreply.github.com>
2020-04-10 09:38:43 -0400

    Add files via upload

--- B_gtex/05_process_dna/modify_json.py
+++ B_gtex/05_process_dna/modify_json.py
@@ -1,1 +1,12 @@
+# In this script, I want to modify the json file. Please see the explanation in the file notes.md for why I am doing this.
+import json
+with open('process_dna_config.json') as json_file:
+    data = json.load(json_file)
+data['all_dna_samples_without_SM'] = []
+for i in data['all_dna_samples']:
+    items = i.split('-')
+    data['all_dna_samples_without_SM'].append(items[0] + '-' + items[1] + '-' + items[2])
+
+with open('process_dna_config.json', 'w') as json_file:
+    json.dump(data, json_file)
 
